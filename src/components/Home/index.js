df09bc22c29dd2ab555eaf4f37bbf5949d8d0dca
carlos xavier <carlinhosxavier@gmail.com>
2022-08-02 14:51:15 -0300

    componente home

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from '../About';
+import Slide from '../Slide';
+
+const Home = () => {
+  return (
+    <>
+    <Slide />
+    <About />
+    </>
+  )
+}
+
+export default Home;
