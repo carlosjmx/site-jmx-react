8182331dc17ebd586581c39859e3dc99959ddfad
carlos xavier <carlinhosxavier@gmail.com>
2022-08-02 14:55:51 -0300

    Componente Contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='container-fluid'>
+      <div className='container'></div>
+    </div>
+  )
+}
+
+export default Contact;
